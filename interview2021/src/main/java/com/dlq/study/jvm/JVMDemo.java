6ed975bf84714490ec1878df89d963d5aaec5ff3
DLQ <1114626450@qq.com>
2021-06-19 22:37:44 +0800

    JOL证明对象头->换成其他对象试试

--- interview2021/src/main/java/com/dlq/study/jvm/JVMDemo.java
+++ interview2021/src/main/java/com/dlq/study/jvm/JVMDemo.java
@@ -11,9 +11,17 @@ import org.openjdk.jol.vm.VM;
  */
 public class JVMDemo {
     public static void main(String[] args) {
-        //VM的细节详细情况
+        /*//VM的细节详细情况
         System.out.println(VM.current().details());
         //所有的对象分配的字节都是8的整数倍
-        System.out.println(VM.current().objectAlignment());
+        System.out.println(VM.current().objectAlignment());*/
+
+        System.out.println(ClassLayout.parseInstance(new Object()).toPrintable());
+        System.out.println(ClassLayout.parseInstance(new Book()).toPrintable());
     }
+}
+
+class Book{
+    private int a = 999 ;
+    private boolean flag = true;
 }
