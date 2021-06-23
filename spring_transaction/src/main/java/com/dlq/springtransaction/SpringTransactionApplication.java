aecee1813122785513d4ed97c379906cc0e7b54e
DLQ <1114626450@qq.com>
2021-06-23 22:27:53 +0800

    SpringBoot事务失效 解决使用注入代理类

--- spring_transaction/src/main/java/com/dlq/springtransaction/SpringTransactionApplication.java
+++ spring_transaction/src/main/java/com/dlq/springtransaction/SpringTransactionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement/*(proxyTargetClass = true)*/ //事务处理
+@EnableTransactionManagement(proxyTargetClass = true) //事务处理
 //@EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.dlq.springtransaction.mapper")
 @SpringBootApplication
